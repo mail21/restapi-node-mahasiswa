b9e916a229bcfc18d2033c72bee62e137f8578d9
Ismail <ismailidn1@gmail.com>
2020-09-06 19:36:48 +0700

    Configuration

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./routes')(app);
+
+app.listen(3000, () => console.log('Listening to port 3000'));
